164cef55af73999827899eb7b34fab846e1e4e7c
Felicia Graciella <75228967+feliciagraciella@users.noreply.github.com>
2024-01-06 21:17:08 +0700

    Initial Commit

---
+++ Perpustakaan-New/Perpustakaan_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perpustakaan_NewApp.swift
+//  Perpustakaan-New
+//
+//  Created by Felicia Graciella on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Perpustakaan_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
