682d7f9788c6e06b47ce8a187db9298dde959ef4
Felicia Graciella <75228967+feliciagraciella@users.noreply.github.com>
2024-01-07 13:34:30 +0700

    s

---
+++ Perpustakaan-New/BookListView.swift
@@ -0,0 +1,18 @@
+//
+//  BookListView.swift
+//  Perpustakaan-New
+//
+//  Created by Felicia Graciella on 07/01/24.
+//
+
+import SwiftUI
+
+struct BookListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookListView()
+}
---
+++ Perpustakaan-New/EditBookView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBookView.swift
+//  Perpustakaan-New
+//
+//  Created by Felicia Graciella on 07/01/24.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBookView()
+}
